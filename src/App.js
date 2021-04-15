9365bc7201a514a5c3e0e77721fb8d6ec71ef731
migiis <mixu2121@hotmail.com>
2021-04-15 13:29:29 +0300

    fetch

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-
-import React from 'react';
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Radiobutton from './components/Radiobutton';
 
 function App() {
 
-const [answer, setAnswer] = React.useState('');
-const [question, setQuestion] = React.useState('');
+const [answer, setAnswer] = useState('');
+const [question, setQuestion] = useState('');
 
 useEffect(() => {
   fetchApi();
 }, []);
 
 const fetchApi = () => {
-  fetch("https://taitokysely.herokuapp.com/")
+  //fetch('https://jsonplaceholder.typicode.com/todos/1')
+  fetch('https://taitokysely.herokuapp.com/')
   .then(response => response.json())
-  .then(data => {
-  setAnswer(data._links.answers.href);
-  //setQuestion(data._links.self.href);
-  })
-  
+  .then(json => console.log(json))
+  //setAnswer(data._links.answers.href);
+  //setQuestion(data._links.questions.href);
+  .catch(err => console.error(err))
 }
 
   return (
     <div className="App">
       <h1>{answer}</h1>
       <h1>{question}</h1>
-      <Radiobutton/>
+      <Radiobutton />
     </div>
   );
 }
