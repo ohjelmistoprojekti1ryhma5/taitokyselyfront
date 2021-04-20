4d6c17bcd9e552a1ca2a0165e95dbfb06e452bc2
Miska Vivolin <miska.vivolin@gmail.com>
2021-04-20 16:30:11 +0300

    fronttiin radiobutton ja textfield

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
-
-import React from 'react';
-import { useEffect } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Radiobutton from './components/Radiobutton';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Textfield from './components/Textfield';
+import Button from '@material-ui/core/Button';
 
 function App() {
 
-const [answer, setAnswer] = React.useState('');
-const [question, setQuestion] = React.useState('');
-
-useEffect(() => {
-  fetchApi();
-}, []);
-
-const fetchApi = () => {
-  fetch("https://taitokysely.herokuapp.com/")
-  .then(response => response.json())
-  .then(data => {
-  setAnswer(data._links.answers.href);
-  //setQuestion(data._links.self.href);
-  })
-  
-}
-
   return (
+
     <div className="App">
-      <h1>{answer}</h1>
-      <h1>{question}</h1>
-      <Radiobutton/>
+      <AppBar position="static" color="secondary" style={{ marginBottom: '30px' }}>
+        <Typography variant="h4">
+          Taitokysely
+        </Typography>
+      </AppBar>
+      
+      <Router>
+        <div>
+          <Link to="/">Radiobutton</Link>{' '}
+          <Link to="/textfield/">Textfield</Link>{' '}
+
+          <Switch>
+            <Route exact path="/" component={Radiobutton} />
+            <Route exact path="/textfield/" component={Textfield} />
+            <Route render={() => <h1>Page not found</h1>} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
--- src/components/Radiobutton.js
+++ src/components/Radiobutton.js
@@ -1,12 +1,46 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 function Radiobutton() {
-    return (
-      <div >
-        Radiobutton
-      </div>
+    
+const [answer, setAnswer] = React.useState('');
+const [question, setQuestion] = React.useState('');
+
+useEffect(() => {
+  fetchApi();
+}, []);
+
+const [value, setValue] = React.useState('vastaus');
+  
+const handleChange = (event) => {
+  setValue(event.target.value);
+  console.log(event.target.value);
+};
+
+const fetchApi = () => {
+  fetch("https://taitokysely.herokuapp.com/")
+  .then(response => response.json())
+  .then(data => {
+  setAnswer(data._links.answers.href);
+  //setQuestion(data._links.self.href);
+  })
+}
+  
+  return (
+    <div>
+      <FormControl component="fieldset">
+        <FormLabel component="legend">Piditkö kyselystä?</FormLabel>
+        <RadioGroup aria-label="vastaus" name="vastaus" value={value} onChange={handleChange}>
+          <FormControlLabel value="kyllä" control={<Radio />} label="Kyllä" />
+          <FormControlLabel value="en" control={<Radio />} label="En" />
+        </RadioGroup>
+      </FormControl>
+    </div>
     );
   }
-  
+
   export default Radiobutton;
