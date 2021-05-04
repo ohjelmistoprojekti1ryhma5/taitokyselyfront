5b9bbbab47c3611e9559bd136b5ef0f0896e3a8f
Tuomo Oila <tuomo.oila@gmail.com>
2021-05-04 22:26:10 +0300

    Install and run prettier with its default rules. See https://prettier.io/docs/en/install.html for further information.

--- src/components/MyButton.js
+++ src/components/MyButton.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import Button from "@material-ui/core/Button";
 
 function MyButton(props) {
-    
-    const handleSave = () => {
-        console.log(props)
-        props.addAnswer()
-    }
-    
-    return(
-        <div>
-            <Button onClick={handleSave} color="primary">
-              Tallenna
-            </Button>
-        </div>
-    )
+  const handleSave = () => {
+    console.log(props);
+    props.addAnswer();
+  };
+
+  return (
+    <div>
+      <Button onClick={handleSave} color="primary">
+        Tallenna
+      </Button>
+    </div>
+  );
 }
 
 export default MyButton;
