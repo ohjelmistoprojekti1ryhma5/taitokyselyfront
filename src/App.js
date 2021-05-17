3dd2c2d23ae42f76713273d725ac789d92289761
Lauri <lauri.vastela@gmail.com>
2021-05-17 18:49:16 +0300

    Demo

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import Radiobutton from './components/Radiobutton';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
-import Textfield from './components/Textfield';
+import Questionradio from './components/Questionradio';
 import Question from './components/Question';
 
 function App() {
@@ -22,14 +21,14 @@ function App() {
         </Typography>
       </AppBar>
       
-      <Router>
+      <Router >
         <div>
-          <Link to="/">Kysely</Link>{' '}
-          <Link to="/textfield/">Anna palautetta!</Link>{' '}
+          <Link to="/question">Textfield</Link>{' '}
+          <Link to="/questionradio">Radio</Link>
 
           <Switch>
-            <Route exact path="/" component={Question} />
-            <Route exact path="/textfield/" component={Textfield} />
+            <Route exact path="/question" component={Question} />
+            <Route exact path="/questionradio" component={Questionradio} />
             <Route render={() => <h1>Page not found</h1>} />
           </Switch>
         </div>
---
+++ src/components/Questionradio.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import Radiobutton from './Radiobutton';
+
+function Questionradio() {
+ 
+    const [question, setQuestion] = useState([]);
+    
+    useEffect(() => {
+        fetchQuestions();
+    }, []);
+ 
+    const fetchQuestions = () => {
+        fetch('https://taitokysely.herokuapp.com/questions')
+        .then(response => response.json())
+        .then(data => setQuestion(data))
+        .catch(err => console.error(err))
+    }
+ 
+    return (
+        <div>
+            <table align="center">
+             <tbody>
+                {
+                 question.map(questions =>
+                  <tr key={questions.questionId}>
+                     <td>{questions.questionName} <Radiobutton/></td>   
+                  </tr>)
+                }
+             </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Questionradio;
